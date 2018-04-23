25b11bfd04fd094e7b89f46bcac40d433b65d4cb
codeteo <ramones.teo@gmail.com>
2018-04-24 00:12:50 +0300

    Extracts basic layout from files

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,15 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'LSAPP')}}</title>
-
-    </head>
-    <body>
-		<h1>Welcome to the App</h1>
-		<p>This is a sample Laravel App</p>
-    </body>
-</html>
+@section('content')
+	<h1>Welcome to Application</h1>
+	<p>This is a sample app made with Laravel</p>
+@endsection
